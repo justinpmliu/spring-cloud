3933908a37ad46d54077458ed6b703fd4f0dca7c
justinliu <nhliupm@aliyun.com>
2020-05-24 12:54:22 +0800

    use new ApiInfo constructor

--- room-services/src/main/java/com/example/roomservices/RoomServicesApplication.java
+++ room-services/src/main/java/com/example/roomservices/RoomServicesApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.service.Contact;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
@@ -11,6 +13,8 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import static springfox.documentation.builders.PathSelectors.any;
+
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -21,9 +25,9 @@ public class RoomServicesApplication {
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2).groupName("Room").select()
                 .apis(RequestHandlerSelectors.basePackage("com.example.room"))
-                .paths(any()).build().apiInfo(new ApiInfo("Room Services",
-                        "A set of services to provide data access to rooms", "1.0.0", null,
-                        "Frank Moley",null, null));
+                .paths(any()).build().apiInfo(
+                    new ApiInfo("Room Services", "A set of services to provide data access to rooms", "1.0.0", null, 
+                    new Contact("Frank Moley", null, null), null, null, Collections.emptyList()));
     }
 
     public static void main(String[] args) {
