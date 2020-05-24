c114c5112e3e1e7c1cce5e8fffc8c3484b90ddee
justinliu <nhliupm@aliyun.com>
2020-05-24 17:32:09 +0800

    remove unused RestTemplate

--- reservation-business-services/src/main/java/com/example/reservationbusinessservices/RoomReservationController.java
+++ reservation-business-services/src/main/java/com/example/reservationbusinessservices/RoomReservationController.java
@@ -5,6 +5,7 @@ import com.example.reservationbusinessservices.domain.Room;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,7 +16,7 @@ public class RoomReservationController {
     private RoomService roomService;
 
     @RequestMapping(value = "/rooms", method = RequestMethod.GET)
-    public List<Room> getAllRooms() {
-        return roomService.findAll(null);
+    public List<Room> getAllRooms(@RequestParam(name="roomNumber", required = false)String roomNumber) {
+        return roomService.findAll(roomNumber);
     }
 }
