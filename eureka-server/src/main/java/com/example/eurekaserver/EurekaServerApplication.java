63ec39894815988aef5133a448eb628a4d35183b
justinliu <nhliupm@aliyun.com>
2020-05-27 16:24:09 +0800

    revise config

--- eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableEurekaServer
 public class EurekaServerApplication {
 
+/*
     private static final String[] PUBLIC_ENDPOINTS = {
             "/",
             "/lastn",
@@ -20,6 +21,7 @@ public class EurekaServerApplication {
             "/eureka/fonts/**",
             "/eureka/images/**"
     };
+*/
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
@@ -31,9 +33,11 @@ public class EurekaServerApplication {
         protected void configure(HttpSecurity http) throws Exception {
             http.csrf().ignoringAntMatchers("/eureka/**");
 
+/*
             http.authorizeRequests()
                     .antMatchers(PUBLIC_ENDPOINTS)
                     .permitAll();
+*/
 
             super.configure(http);
         }
