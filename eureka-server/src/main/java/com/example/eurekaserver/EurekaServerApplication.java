a92b1c8d0a27f6b3555c2d7706f1f403fce8b919
justinliu <nhliupm@aliyun.com>
2020-05-27 15:47:10 +0800

    modify eureka configuration

--- eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -3,13 +3,40 @@ package com.example.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
 
+    private static final String[] PUBLIC_ENDPOINTS = {
+            "/",
+            "/lastn",
+            "/eureka/css/**",
+            "/eureka/js/**",
+            "/*.ico",
+            "/eureka/fonts/**",
+            "/eureka/images/**"
+    };
+
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+    @EnableWebSecurity
+    class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().ignoringAntMatchers("/eureka/**");
+
+            http.authorizeRequests()
+                    .antMatchers(PUBLIC_ENDPOINTS)
+                    .permitAll();
+
+            super.configure(http);
+        }
     }
 
 }
