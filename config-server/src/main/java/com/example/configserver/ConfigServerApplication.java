2974e527fcd4569819ab64e28d23c54cefec8899
justinliu <nhliupm@aliyun.com>
2020-05-29 13:33:01 +0800

    add prometheus to eureka and config server

--- config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
@@ -1,15 +1,46 @@
 package com.example.configserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
 
+    @Value("${management.endpoints.web.exposure.include}#{T(java.util.Collections).emptyList()}")
+    private List<String> managementEndpointsInclude;
+
+
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+    @EnableWebSecurity
+    class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().ignoringAntMatchers("/eureka/**");
+
+            String[] permittedEndpoints = Stream.of(
+                    managementEndpointsInclude.stream().map(e -> "/actuator/" + e))
+                    .flatMap(Function.identity())
+                    .toArray(String[]::new);
+
+            http.authorizeRequests()
+                    .antMatchers(permittedEndpoints)
+                    .permitAll();
+
+            super.configure(http);
+        }
     }
 
 }
--- eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -1,5 +1,6 @@
 package com.example.eurekaserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -7,12 +8,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
 
+    private static final String[] EUREKA_PUBLIC_ENDPOINTS = {
 /*
-    private static final String[] PUBLIC_ENDPOINTS = {
             "/",
             "/lastn",
             "/eureka/css/**",
@@ -20,8 +25,11 @@ public class EurekaServerApplication {
             "/*.ico",
             "/eureka/fonts/**",
             "/eureka/images/**"
-    };
 */
+    };
+
+    @Value("${management.endpoints.web.exposure.include}#{T(java.util.Collections).emptyList()}")
+    private List<String> managementEndpointsInclude;
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
@@ -33,11 +41,15 @@ public class EurekaServerApplication {
         protected void configure(HttpSecurity http) throws Exception {
             http.csrf().ignoringAntMatchers("/eureka/**");
 
-/*
+            String[] permittedEndpoints = Stream.of(
+                    Stream.of(EUREKA_PUBLIC_ENDPOINTS),
+                    managementEndpointsInclude.stream().map(e -> "/actuator/" + e))
+                    .flatMap(Function.identity())
+                    .toArray(String[]::new);
+
             http.authorizeRequests()
-                    .antMatchers(PUBLIC_ENDPOINTS)
+                    .antMatchers(permittedEndpoints)
                     .permitAll();
-*/
 
             super.configure(http);
         }
