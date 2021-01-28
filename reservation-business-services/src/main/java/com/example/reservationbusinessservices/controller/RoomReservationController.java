f51256e59ac652b7ac46fd03d1f5c044d82d8e10
justinliu <nhliupm@aliyun.com>
2021-01-28 17:09:02 +0800

    use mixin to convert json field name

--- reservation-business-services/src/main/java/com/example/reservationbusinessservices/controller/RoomReservationController.java
+++ reservation-business-services/src/main/java/com/example/reservationbusinessservices/controller/RoomReservationController.java
@@ -2,6 +2,10 @@ package com.example.reservationbusinessservices.controller;
 
 import com.example.reservationbusinessservices.client.RoomService;
 import com.example.reservationbusinessservices.domain.Room;
+import com.example.reservationbusinessservices.domain.RoomMixin;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,12 +15,25 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Slf4j
 public class RoomReservationController {
     @Autowired
     private RoomService roomService;
 
     @RequestMapping(value = "/rooms", method = RequestMethod.GET)
     public List<Room> getAllRooms(@RequestParam(name="roomNumber", required = false)String roomNumber) {
-        return roomService.findAll(roomNumber);
+        List<Room> rooms = roomService.findAll(roomNumber);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.addMixIn(Room.class, RoomMixin.class);
+
+        try {
+            String json = mapper.writeValueAsString(rooms);
+            log.info(json);
+        } catch (JsonProcessingException e) {
+            log.error(e.getMessage(), e);
+        }
+
+        return rooms;
     }
 }
--- reservation-business-services/src/main/java/com/example/reservationbusinessservices/domain/RoomMixin.java
+++ reservation-business-services/src/main/java/com/example/reservationbusinessservices/domain/RoomMixin.java
@@ -1,9 +1,11 @@
 package com.example.reservationbusinessservices.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
-public class Room {
+public abstract class RoomMixin {
+    @JsonProperty("room_id")
     private String id;
     private String name;
     private String roomNumber;
